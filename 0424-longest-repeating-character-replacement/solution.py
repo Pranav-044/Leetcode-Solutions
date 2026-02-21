64a8b6670e6e5816468079f47260a0f6b2ac1901
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-02-21 11:36:05 +0000

    Sync LeetCode submission Runtime - 67 ms (96.27%), Memory - 19.7 MB (49.70%)

--- 0424-longest-repeating-character-replacement/solution.py
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -1,19 +1,21 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l=0
-        length=0
         dictionary={}
-        max_f=0
         maximum=0
+        max_f=0
         for i in range(len(s)):
             length=i-l+1
-            dictionary[s[i]]=dictionary.get(s[i],0)+1
+            dictionary[s[i]] = dictionary.get(s[i],0)+1
             max_f=max(max_f,dictionary[s[i]])
-            val=length-max_f
-            if(val<=k):
+            if(length-max_f<=k):
                 maximum=max(maximum,length)
             else:
                 dictionary[s[l]]-=1
                 l+=1
         return maximum
+            
+
+
+
         
