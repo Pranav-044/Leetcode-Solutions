9c3bb0fc32f75fa522faab1eacc60b32f238e452
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-05-29 09:47:49 +0000

    Sync LeetCode submission Runtime - 12 ms (28.43%), Memory - 19.4 MB (17.83%)

---
+++ 0009-palindrome-number/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        k=str(x)
+        if k[::] == k[::-1]:
+            return True
+        else:
+            return False
+        
