1677d1e55f057561edc8241635cfefa2da392e52
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-06-01 13:25:14 +0000

    Sync LeetCode submission Runtime - 16 ms (28.29%), Memory - 24.1 MB (9.49%)

--- 0049-group-anagrams/solution.py
+++ 0049-group-anagrams/solution.py
@@ -1,11 +1,43 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dict1={}
         dictionary=defaultdict(list)
-        for s in strs:
+        for i in strs:
             array=[0]*26
-            for i in s:
-                array[ord(i)-ord("a")]+=1
-            dictionary[tuple(array)].append(s)
+            for j in i:
+                array[ord(j)-ord("a")]+=1
+            dictionary[tuple(array)].append(i)
         return list(dictionary.values())
+
         
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# dict1={}
+#         dictionary=defaultdict(list)
+#         for s in strs:
+#             array=[0]*26
+#             for i in s:
+#                 array[ord(i)-ord("a")]+=1
+#             dictionary[tuple(array)].append(s)
+#         return list(dictionary.values())
