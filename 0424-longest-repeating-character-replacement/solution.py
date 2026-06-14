9779bbe42c6e494b0c530aedd4691099f34d3143
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-06-14 11:59:19 +0000

    Sync LeetCode submission Runtime - 71 ms (94.23%), Memory - 19.7 MB (14.94%)

--- 0424-longest-repeating-character-replacement/solution.py
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -1,20 +1,75 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l=0
-        max_f=0
+        # maximum=0
+        # l=0
+        # arr=[0]*26
+        # for i in range(len(s)):
+        #     arr[ord(s[i])-65]+=1
+        #     while((i-l+1)-max(arr)>k):
+        #         arr[ord(s[l])-65]-=1
+        #         l+=1
+        #     maximum=max(maximum,i-l+1)
+        # return maximum        
         dictionary={}
+        length=0
+        l=0
+        longest=0
         maximum=0
         for i in range(len(s)):
             length=i-l+1
             dictionary[s[i]]=dictionary.get(s[i],0)+1
-            max_f=max(dictionary[s[i]],max_f)
-            val=length-max_f
+            maximum=max(maximum,dictionary[s[i]])
+            val=length-maximum
             if(val<=k):
-                maximum=max(maximum,length)
-            else:   
-                dictionary[s[l]]-=1 
+                longest=max(longest,length)
+            else:
+                dictionary[s[l]]-=1
                 l+=1
-        return maximum                
+        return longest       
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # l=0
+        # max_f=0
+        # dictionary={}
+        # maximum=0
+        # for i in range(len(s)):
+        #     length=i-l+1
+        #     dictionary[s[i]]=dictionary.get(s[i],0)+1
+        #     max_f=max(dictionary[s[i]],max_f)
+        #     val=length-max_f
+        #     if(val<=k):
+        #         maximum=max(maximum,length)
+        #     else:   
+        #         dictionary[s[l]]-=1 
+        #         l+=1
+        # return maximum                
 
             
         
