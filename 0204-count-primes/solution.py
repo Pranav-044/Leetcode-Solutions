cce6d68ec28ad87640aeed43aa35f14980ee8872
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-06-24 17:14:40 +0000

    Sync LeetCode submission Runtime - 1628 ms (34.51%), Memory - 58 MB (70.24%)

---
+++ 0204-count-primes/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        count=0
+        if n<2:
+            return 0
+        else:
+            is_prime = [True] * (n)
+            is_prime[0] = is_prime[1] = False
+
+            for i in range(2, int(n ** 0.5) + 1):
+                if is_prime[i]:
+                    for j in range(i * i, n, i):
+                        is_prime[j] = False
+
+            for i in range(2, n):
+                if is_prime[i]:
+                    count+=1
+            return count
+            
