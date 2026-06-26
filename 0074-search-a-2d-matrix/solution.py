1b4de35fb6501784b7331c3220d4d5a9936ba129
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-06-26 06:17:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (38.67%)

--- 0074-search-a-2d-matrix/solution.py
+++ 0074-search-a-2d-matrix/solution.py
@@ -1,26 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        top=0
-        bot=len(matrix)-1
-        while top<=bot:
-            mid=(top+bot)//2
-            if (matrix[mid][-1]<target):
-                top=mid+1
-            elif(matrix[mid][0]>target):
-                bot=mid-1
-            else:
-                break
-        if(not (top<=bot)):
-            return False
         l=0
-        r=len(matrix[mid])
+        r=len(matrix)-1
+        check=False
         while l<=r:
-            temp_mid=(l+r)//2
-            if(matrix[mid][temp_mid]==target):
-                return True
-            elif(matrix[mid][temp_mid]>target):
-                r=temp_mid-1
-            elif(matrix[mid][temp_mid]<target):
-                l=temp_mid+1
-        return False
+            mid=(l+r)//2
+            if(matrix[mid][-1]>=target and matrix[mid][0]<=target):
+                check=solve(0,len(matrix[mid])-1,matrix[mid],target)
+                break
+            
+            elif(matrix[mid][-1]<target):
+                l=mid+1
+            else:
+                r=mid-1
+        return check
         
+
+def solve(l,r,arr,target):
+    while(l<=r):
+        mid=(l+r)//2
+        if(arr[mid]<target):
+            l=mid+1
+        elif(arr[mid]>target):
+            r=mid-1
+        else:
+            return True
+    return False
+
