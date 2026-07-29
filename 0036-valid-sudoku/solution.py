af7efcf3f2a6aaaca46354b6ab1585ccbf910347
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-07-29 14:50:44 +0000

    Sync LeetCode submission Runtime - 3 ms (74.06%), Memory - 19.2 MB (94.64%)

--- 0036-valid-sudoku/solution.py
+++ 0036-valid-sudoku/solution.py
@@ -1,34 +1,29 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(9):
-            s=set()
-            for j in range(9):
-                if board[i][j] in s:
+        for i in range(len(board)):
+            setter=set()
+            for j in range(len(board[0])):
+                if board[i][j] in setter:
                     return False
-                elif board[i][j]!=".":
-                    s.add(board[i][j])
-        
-        for i in range(9):
-            s=set()
-            for j in range(9):
-                if board[j][i] in s:
+                elif board[i][j] !=".":
+                    setter.add(board[i][j])
+        for i in range(len(board[0])):
+            setter=set()
+            for j in range(len(board)):
+                if board[j][i] in setter:
                     return False
-                elif board[j][i]!=".":
-                    s.add(board[j][i])
-        
-        
-        
-        indices=[(0,0),(0,3),(0,6),(3,0),(3,3),(3,6),(6,0),(6,3),(6,6)]
-        for j,k in indices:
-            s=set()
-            for i in range(j,j+3):
-                
-                for m in range(k,k+3):
-                    if board[i][m] in s:
+                elif board[j][i] !=".":
+                    setter.add(board[j][i])
+        start=[[0,0],[0,3],[0,6],[3,0],[3,3],[3,6],[6,0],[6,3],[6,6]]
+        for i,j in start:
+            setter=set()
+            for row in range(i,i+3):
+                for cols in range(j,j+3):
+                    if board[row][cols] in setter:
                         return False
-                    elif board[i][m]!=".":
-                        s.add(board[i][m])
+                    elif board[row][cols] !=".":
+                        setter.add(board[row][cols])
+        
         return True
 
-        
 
