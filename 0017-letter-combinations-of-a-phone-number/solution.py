8f2100e30e94a17a2f04b6e963ff1b92cff207a4
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-08-05 16:52:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (9.27%)

---
+++ 0017-letter-combinations-of-a-phone-number/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        letter_map={2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"prqs",8:"tuv",9:"wxyz"}
+        if digits == "":
+            return []
+        ans,sol=[],[]
+        n=len(digits)
+        def backtrack(i=0):
+            if i == n:
+                ans.append("".join(sol))
+                return
+            for letter in letter_map[int(digits[i])]:
+                sol.append(letter)
+                backtrack(i+1)
+                sol.pop()
+        backtrack(0)
+        return ans
+
+
+        
+        
