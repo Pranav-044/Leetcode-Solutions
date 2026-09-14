d8b0c0311a9f7d11ca584e0c679f27364cf90305
Pranav Parimi <105921183+Pranav-044@users.noreply.github.com>
2026-09-14 10:59:16 +0000

    Sync LeetCode submission Runtime - 131 ms (5.56%), Memory - 20.1 MB (9.30%)

---
+++ 1938-minimum-operations-to-make-the-array-increasing/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        count=0
+        for i in range(1,len(nums)):
+            if(nums[i]<nums[i-1]):
+                count+=(nums[i-1]-nums[i])+1
+                nums[i]+= (nums[i-1]-nums[i])+1
+            elif(nums[i] == nums[i-1]):
+                nums[i]+=1
+                count+=1
+            print(count)
+        return count 
+        
